f8199daf83f0bff7fd1afa3020c96e24a834d2cc
brianbrix <mokandubrian@gmail.com>
2022-05-12 14:21:21 +0300

    First commit

---
+++ src/main/java/com/brianbrix/demo/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.brianbrix.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping("/")
+    String test()
+    {
+        return "Test successful";
+    }
+}
---
+++ src/test/java/com/brianbrix/demo/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianbrix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
